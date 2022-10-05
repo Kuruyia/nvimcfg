8d09c1815683722f589268461475cbe92ed8bee7
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2022-10-05 11:14:35 +0200

    Add telescope-fzf-native.nvim as a dependency of telescope.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,15 +51,16 @@ return require('packer').startup(function()
     -- https://github.com/nvim-telescope/telescope.nvim
     use {
         'nvim-telescope/telescope.nvim',
-        requires = { 'nvim-lua/plenary.nvim' },
+        requires = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+            }
+        },
         config = function()
             require('config.telescope')
         end
-    }
-
-    use {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
     }
 
     -- Bufferline

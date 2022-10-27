acc34c932c565db39aea534e047716b54fc96e90
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2022-10-27 11:34:13 +0200

    lsp: use default_capabilities instead of update_capabilities

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,5 @@
 -- LSP servers
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- Lua LSP server

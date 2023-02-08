43ca57d0ea5f7311b9936a12680ff8fdeb0e6bea
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2023-02-08 20:46:59 +0100

    feat(bufferline): hide close buttons, show on hover

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -1,6 +1,13 @@
 -- Bufferline
 require('bufferline').setup({
     options = {
-        mode = 'tabs'
+        hover = {
+            enabled = true,
+            delay = 200,
+            reveal = {'close'}
+        },
+        mode = 'tabs',
+        show_buffer_close_icons = false,
+        show_close_icon = false
     }
 })

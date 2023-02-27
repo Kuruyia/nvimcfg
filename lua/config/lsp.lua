61766b687cf4eccf6a2b469c894108825e3e8b50
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2023-02-27 10:54:33 +0100

    feat(lsp): check if an LSP is installed before configuring it

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -7,10 +7,43 @@ local on_attach = function(client)
     require('illuminate').on_attach(client)
 end
 
+-- Check if a LSP is installed
+local get_default_config = function(lsp_name)
+    local server_config = require('lspconfig.server_configurations.'..lsp_name)
+
+    if server_config == nil then
+        return nil
+    end
+
+    return server_config.default_config
+end
+
+local check_lsp_installed = function(lsp_name)
+    -- Get the default config for the LSP
+    local lsp_config = get_default_config(lsp_name)
+
+    if lsp_config == nil then
+        vim.api.nvim_notify('Unable to check if the "'..lsp_config.name..'" LSP is installed.', vim.log.levels.ERROR, {})
+        return false
+    end
+
+    if lsp_config.cmd == nil then
+        vim.api.nvim_notify('Unable to check the cmd of the "'..lsp_config.name..'" LSP.', vim.log.levels.ERROR, {})
+        return false
+    end
+
+    -- Check if the executable is present
+    for _, v in pairs(lsp_config.cmd) do
+        if vim.fn.executable(v) == 1 then
+            return true
+        end
+    end
+end
+
 -- For a list of LSP servers: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
 -- Lua LSP server
-if lspconfig.lua_ls then
+if lspconfig.lua_ls and check_lsp_installed(lspconfig.lua_ls.name) then
     local lua_ls_settings = {
         Lua = {
             telemetry = {
@@ -42,7 +75,7 @@ if lspconfig.lua_ls then
 end
 
 -- C/C++ LSP server
-if lspconfig.clangd then
+if lspconfig.clangd and check_lsp_installed(lspconfig.clangd.name) then
     lspconfig.clangd.setup({
         capabilities = capabilities,
         on_attach = on_attach
@@ -64,14 +97,14 @@ if (vim.loop.os_uname().sysname == "Linux") then
     }
 end
 
-if lspconfig.ltex then
+if lspconfig.ltex and check_lsp_installed(lspconfig.ltex.name) then
     lspconfig.ltex.setup({
         capabilities = capabilities,
         on_attach = on_attach
     })
 end
 
-if lspconfig.texlab then
+if lspconfig.texlab and check_lsp_installed(lspconfig.texlab.name) then
     lspconfig.texlab.setup({
         capabilities = capabilities,
         on_attach = on_attach,
@@ -91,7 +124,7 @@ if lspconfig.texlab then
 end
 
 -- TypeScript / JavaScript
-if lspconfig.tsserver then
+if lspconfig.tsserver and check_lsp_installed(lspconfig.tsserver.name) then
     lspconfig.tsserver.setup({
         capabilities = capabilities,
         on_attach = on_attach
@@ -99,7 +132,7 @@ if lspconfig.tsserver then
 end
 
 -- Vuejs
-if lspconfig.vuels then
+if lspconfig.vuels and check_lsp_installed(lspconfig.vuels.name) then
     lspconfig.vuels.setup({
         capabilities = capabilities,
         on_attach = on_attach
@@ -107,7 +140,7 @@ if lspconfig.vuels then
 end
 
 -- Python
-if lspconfig.pyright then
+if lspconfig.pyright and check_lsp_installed(lspconfig.pyright.name) then
     lspconfig.pyright.setup({
         capabilities = capabilities,
         on_attach = on_attach
@@ -115,7 +148,7 @@ if lspconfig.pyright then
 end
 
 -- Golang
-if lspconfig.gopls then
+if lspconfig.gopls and check_lsp_installed(lspconfig.gopls.name) then
     lspconfig.gopls.setup({
         capabilities = capabilities,
         on_attach = on_attach
@@ -123,7 +156,7 @@ if lspconfig.gopls then
 end
 
 -- Terraform
-if lspconfig.terraformls then
+if lspconfig.terraformls and check_lsp_installed(lspconfig.terraformls.name) then
     lspconfig.terraformls.setup({
         capabilities = capabilities,
         on_attach = on_attach
@@ -131,7 +164,7 @@ if lspconfig.terraformls then
 end
 
 -- Rust
-if lspconfig.rust_analyzer then
+if lspconfig.rust_analyzer and check_lsp_installed(lspconfig.rust_analyzer.name) then
     lspconfig.rust_analyzer.setup({
         capabilities = capabilities,
         on_attach = on_attach

331bd792b3c82414773a5473ca5abf64146b9280
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2023-02-27 16:03:54 +0100

    feat: remove trouble keymaps

--- lua/config/trouble.lua
+++ lua/config/trouble.lua
@@ -1,6 +1,2 @@
 -- Trouble
 require('trouble').setup()
-
--- Vim keymaps
-vim.keymap.set('n', 'f', '<CMD>TroubleToggle lsp_references<CR>')
-vim.keymap.set('n', 'F', '<CMD>TroubleToggle lsp_definitions<CR>')

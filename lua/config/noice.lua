ddec4ca27773eb01503c2f973603034240b1d303
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2023-12-02 18:08:30 +0100

    feat(noice): add excluded lsp clients and border to doc window

--- lua/config/noice.lua
+++ lua/config/noice.lua
@@ -1,4 +1,6 @@
 -- Noice
+local excluded_lsp_client_progress_notification = { 'ltex' }
+
 require('noice').setup({
   cmdline = {
     format = {
@@ -15,6 +17,26 @@ require('noice').setup({
   },
   presets = {
     long_message_to_split = true,
-    lsp_doc_border = false,
+    lsp_doc_border = true,
+  },
+  routes = {
+    {
+      filter = {
+        event = 'lsp',
+        kind = 'progress',
+        cond = function(message)
+          local client = vim.tbl_get(message.opts, 'progress', 'client')
+
+          for _, v in pairs(excluded_lsp_client_progress_notification) do
+            if client == v then
+              return true
+            end
+          end
+
+          return false
+        end,
+      },
+      opts = { skip = true },
+    },
   },
 })

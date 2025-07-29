87be3e3f4cf0035e04143e2de3c1fbcbe8bde642
Kuruyia <github@kuruyia.net>
2025-07-29 11:54:37 +0200

    feat(diffview): add keybinds to close the view
    Signed-off-by: Kuruyia <github@kuruyia.net>

--- lua/config/neogit.lua
+++ lua/config/neogit.lua
@@ -7,3 +7,12 @@ require('neogit').setup({
     },
   },
 })
+
+-- Diffview
+require('diffview').setup({
+  key_bindings = {
+    file_history_panel = { q = '<Cmd>DiffviewClose<CR>' },
+    file_panel = { q = '<Cmd>DiffviewClose<CR>' },
+    view = { q = '<Cmd>DiffviewClose<CR>' },
+  },
+})
